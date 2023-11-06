80338bd0aa56fd32e59a81cf6a69f0a69949c5e3
RaykoEZ <onion55zh@gmail.com>
2023-11-06 00:18:19 +0000

    Blocking out animations for each phone screen

---
+++ KeepQuiet/Assets/src/PhoneScreenTransitionHandler.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+
+namespace Curry.Explore
+{
+    public class PhoneScreenTransitionHandler : MonoBehaviour
+    {
+        [SerializeField] HideableUITrigger m_home = default;
+        [SerializeField] HideableUITrigger m_settings = default;
+        [SerializeField] HideableUITrigger m_contact = default;
+        [SerializeField] HideableUITrigger m_chat = default;
+        [SerializeField] HideableUITrigger m_call = default;
+        [SerializeField] HideableUITrigger m_lock = default;
+        HideableUITrigger m_currentlyShowing;
+        void Start() 
+        {
+            ToHome();
+        }
+        //TODO: secret app for later
+        public void ToHome() 
+        {
+            TransitionTo(m_home);
+        }
+        public void ToSettings() 
+        {
+            TransitionTo(m_settings);
+        }
+        public void ToContact()
+        {
+            TransitionTo(m_contact);
+        }
+        public void ToChat() 
+        {
+            TransitionTo(m_chat);
+        }
+        public void ToDial() 
+        {
+            TransitionTo(m_call);
+        }
+        public void LockScreen() 
+        {
+            m_lock?.Show();
+        }
+        public void UnlockScreen() 
+        {
+            m_lock?.Hide();
+        }
+        void TransitionTo(HideableUITrigger goTo) 
+        {
+            m_currentlyShowing?.Hide();
+            m_currentlyShowing = goTo;
+            m_currentlyShowing?.Show();
+        }
+    }
+}
