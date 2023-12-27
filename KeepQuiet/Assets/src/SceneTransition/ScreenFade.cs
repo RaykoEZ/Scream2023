5d197657ce8e2ec9aa99d7bf973bb79c15e71559
RaykoEZ <onion55zh@gmail.com>
2023-12-27 01:30:34 +0000

    Organizing assets and implementing environment in scene and environment transitions scripts

---
+++ KeepQuiet/Assets/src/SceneTransition/ScreenFade.cs
@@ -0,0 +1,25 @@
+﻿using Curry.Explore;
+using System.Collections;
+using UnityEngine;
+
+public class ScreenFade : HideableUI
+{
+    [SerializeField] AnimationClip m_fadeInClip = default;
+    bool m_inProgress = false;
+    public void StartFade(float holdFadeLength = 0.5f)
+    {
+        if (!m_inProgress) 
+        {
+            m_inProgress = true;
+            StartCoroutine(Fade_Internal(holdFadeLength));
+        }
+    }
+    IEnumerator Fade_Internal(float holdLength)
+    {
+        Show();
+        yield return new WaitForSeconds(m_fadeInClip.length);
+        yield return new WaitForSeconds(holdLength);
+        Hide();
+        m_inProgress = false;
+    }
+}
