7b71166118de79a8e904f5035a85978452329ac3
RaykoEZ <onion55zh@gmail.com>
2024-02-27 22:08:39 +0000

    Short edit to clean save data code

--- KeepQuiet/Assets/src/SceneTransition/GameStateFileHandler.cs
+++ KeepQuiet/Assets/src/SceneTransition/GameStateFileHandler.cs
@@ -23,6 +23,11 @@ public class GameStateFileHandler : MonoBehaviour
         m_onsaveGameState?.Init();
         LoadStates();
     }
+    private void OnApplicationQuit()
+    {            
+        // Autosave on quitting
+        SaveStates(m_current);
+    }
     public void LoadGameState()
     {
         Dictionary<string, object> payload = new Dictionary<string, object>
@@ -34,27 +39,34 @@ public class GameStateFileHandler : MonoBehaviour
     {
         LoadGameState();
     }
-    public void OnGameSave(EventInfo info) 
+    // sets valid incoming save data
+    void HandleSaveData(EventInfo info) 
     {
         Dictionary<string, object> payload = info.Payload;
         if (payload == null) return;
         if (payload.TryGetValue("save", out object result)
             && result is SaveData save)
         {
-            // Autosave
-            SaveStates(save);
+            m_current = save;
         }
+    }
+    public void OnGameSave(EventInfo info) 
+    {
+        HandleSaveData(info);
+        // Autosave
+        SaveStates(m_current);
         // Do on finish callback
         info.OnFinishedCallback?.Invoke();
     }
     public void OnQuitGame(EventInfo info) 
     {
-        OnGameSave(info);
+        HandleSaveData(info);
         // Quit the game
 #if UNITY_STANDALONE
         Application.Quit();
 #endif
 #if UNITY_EDITOR
+        SaveStates(m_current);
         UnityEditor.EditorApplication.isPlaying = false;
 #endif
     }
@@ -75,7 +87,6 @@ public class GameStateFileHandler : MonoBehaviour
     }
     protected void SaveStates(SaveData save) 
     {
-        m_current = save;
         // if we are saving after finish an ending, increment new gamw counter
         SaveData newSave = new SaveData(save);
         string json = JsonConvert.SerializeObject(newSave);
