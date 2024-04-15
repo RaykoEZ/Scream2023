d4f6362abbeebe69b64ac3ba7c1ecef681115e5d
RaykoEZ <onion55zh@gmail.com>
2024-04-16 00:52:05 +0100

    custom postprocess track for timeline

---
+++ KeepQuiet/Assets/src/Interaction/CustomTimelineAssets/PostProcess/PostProcessBehaviour.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine.Playables;
+using UnityEngine.Rendering;
+
+public class PostProcessBehaviour : PlayableBehaviour
+{
+    public float VolumeWeight;
+    public VolumeProfile PostprocessSetting;
+}
--- KeepQuiet/Assets/src/Interaction/CustomTimelineAssets/Subtitle/SubtitleBehaviour.cs
+++ KeepQuiet/Assets/src/Interaction/CustomTimelineAssets/Subtitle/SubtitleBehaviour.cs
@@ -6,10 +6,4 @@ public class SubtitleBehaviour : PlayableBehaviour
 {
     public Color TextColour;
     public string SubtitleText;
-    public override void ProcessFrame(Playable playable, FrameData info, object playerData)
-    {
-        TextMeshProUGUI text = playerData as TextMeshProUGUI;
-        text.text = SubtitleText;
-        text.color = new Color(TextColour.r, TextColour.g, TextColour.b, info.weight);
-    }
 }
