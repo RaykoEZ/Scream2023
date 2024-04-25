ffef4da97a79ce39c97cc9d9504528a2a141282f
RaykoEZ <onion55zh@gmail.com>
2024-04-25 02:11:22 +0100

    Adding Messages and More tutorials

--- KeepQuiet/Assets/src/Interaction/Call/AudioSettingHandler.cs
+++ KeepQuiet/Assets/src/Interaction/Call/AudioSettingHandler.cs
@@ -3,39 +3,13 @@ using System.Collections.Generic;
 using Curry.Events;
 using UnityEngine.Audio;
 
-public class AudioSettingHandler : MonoBehaviour , ISettingUpdateListener<PhoneSettings>
+public class AudioSettingHandler : MonoBehaviour
 {
-    [SerializeField] PhoneSettings m_setting = default;
-    [SerializeField] List<AudioSource> m_audio = default;
     [SerializeField] CurryGameEventTrigger m_playRain = default;
     [SerializeField] CurryGameEventTrigger m_stopRain = default;
     [SerializeField] CurryGameEventTrigger m_playBgm = default;
     [SerializeField] CurryGameEventTrigger m_changeBgm = default;
     [SerializeField] CurryGameEventTrigger m_stopBgm = default;
-    void OnEnable()
-    {
-        m_setting?.Listen(this);
-    }
-    void OnDisable()
-    {
-        m_setting?.Unlisten(this);
-    }
-    void Start() 
-    {
-        foreach (var item in m_audio)
-        {
-            item.volume = m_setting.GetVolume();
-
-        }
-    }
-    public void OnUpdate(PhoneSettings updated)
-    {
-        foreach (var item in m_audio)
-        {
-            item.volume = updated.GetVolume();
-
-        }
-    }
     public void PlayBgm(AudioClip bgm)
     {
         AudioInfo info = new AudioInfo(bgm, true);
---
+++ KeepQuiet/Assets/src/Interaction/Call/TutorialManager.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class TutorialManager : MonoBehaviour 
+{
+    [SerializeField] GameStateManager m_gameState = default;
+    [SerializeField] TutorialCollection m_uiNavigation = default;
+    [SerializeField] TutorialCollection m_tools = default;
+    [SerializeField] TutorialCollection m_newGame = default;
+    TutorialCollection m_current;
+    public void Navigation() 
+    {
+        m_uiNavigation?.Begin();
+        m_current = m_uiNavigation;
+    }
+    public void Tools() 
+    {
+        m_tools?.Begin();
+        m_current = m_tools;
+
+    }
+    public void NewGame() 
+    {
+        m_newGame?.Begin();
+        m_current = m_newGame;
+    }
+    public void NextStep() 
+    {
+        m_current?.NextTutorialStep();
+    }
+
+}
