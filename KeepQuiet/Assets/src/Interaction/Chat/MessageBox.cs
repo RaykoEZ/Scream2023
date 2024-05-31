c0393a0de475e924b38c8f35105c405ff994e0b2
RaykoEZ <onion55zh@gmail.com>
2024-05-31 16:34:06 +0100

    Added scalable text message bubble

--- KeepQuiet/Assets/src/Interaction/Chat/MessageBox.cs
+++ KeepQuiet/Assets/src/Interaction/Chat/MessageBox.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class MessageBox : HideableUI
 {
     [SerializeField] TextMeshProUGUI m_content = default;
+    protected bool m_typing = false;
     public void Init(string name, string content)
     {
         m_content.text = content;
