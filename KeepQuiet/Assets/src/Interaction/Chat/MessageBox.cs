068d5dad7c5e7b1167a52cc23d8620885694fc0d
RaykoEZ <onion55zh@gmail.com>
2024-05-31 02:17:54 +0100

    Added chat text bubbles and animations, need to set them to fit with text size

--- KeepQuiet/Assets/src/Interaction/Chat/MessageBox.cs
+++ KeepQuiet/Assets/src/Interaction/Chat/MessageBox.cs
@@ -4,11 +4,9 @@ using UnityEngine;
 // A class to display a text message
 public class MessageBox : HideableUI
 {
-    [SerializeField] TextMeshProUGUI m_name = default;
     [SerializeField] TextMeshProUGUI m_content = default;
     public void Init(string name, string content)
     {
-        m_name.text = name;
         m_content.text = content;
     }
 }
