f53d820ee2b4d8519b16261d575202464db9caf1
RaykoEZ <onion55zh@gmail.com>
2024-08-07 04:01:36 +0100

    added talking animations to aria closeup

--- KeepQuiet/Assets/src/SceneTransition/AriaCloseupHandler.cs
+++ KeepQuiet/Assets/src/SceneTransition/AriaCloseupHandler.cs
@@ -1,17 +1,12 @@
 ﻿using UnityEngine;
-public enum MouthState 
-{ 
-    Closed = 0,
-    Open = 1,
-    Half = 2
-}
+using UnityEngine.Playables;
+
 public class AriaCloseupHandler : MonoBehaviour 
 {
     [SerializeField] Animator m_anim = default;
     public void EnterScene() 
     {
         m_anim.SetTrigger("enter");
-
     }
     public void ExitScene() 
     {
@@ -25,26 +20,9 @@ public class AriaCloseupHandler : MonoBehaviour
     {
         m_anim.SetTrigger("angry");
     }
-    public void Smug(int mouthState) 
+    public void Smug() 
     {
-        Smug((MouthState)mouthState);
-    }
-    public void Smug(MouthState mouth) 
-    {
-        switch (mouth)
-        {
-            case MouthState.Closed:
-                m_anim.SetTrigger("smugClose");
-                break;
-            case MouthState.Open:
-                m_anim.SetTrigger("smugOpen");
-                break;
-            case MouthState.Half:
-                m_anim.SetTrigger("smugHalf");
-                break;
-            default:
-                break;
-        }
+        m_anim.SetTrigger("smug");
     }
     public void Scary() 
     {
@@ -57,13 +35,5 @@ public class AriaCloseupHandler : MonoBehaviour
     public void Shadow() 
     {
         m_anim.SetTrigger("hideScary");
-    }
-    public void LaughingShadow() 
-    {
-        m_anim.SetTrigger("laughShadow");
-    }
-    public void BloodyShadow() 
-    {
-        m_anim.SetTrigger("bloodShadow");
     }
 }
