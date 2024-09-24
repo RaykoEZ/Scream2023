3c134cb6558742c35b37414a511995e55bfcf3fe
RaykoEZ <onion55zh@gmail.com>
2024-09-24 02:15:20 +0100

    Testing ui raycast interactions and simplifying story dialogue

--- KeepQuiet/Assets/src/SceneTransition/View/ThoughtUIManager.cs
+++ KeepQuiet/Assets/src/SceneTransition/View/ThoughtUIManager.cs
@@ -5,36 +5,36 @@ public class ThoughtUIManager : MonoBehaviour
 {
     [SerializeField] HideableUI m_thoughtInventory = default;
     [SerializeField] TemporaryInputAction m_dragCancel = default;
-    HideableUI m_currentDisplayRef;
+    HideableUI m_currentDropzone;
     DraggableObject m_dragging;
     public void EnableDropzone(ThoughtDropZone dropzone)
     {
         if (dropzone == null) return;
-        m_currentDisplayRef = dropzone.DisplayUI;
-    }
-    public void DisableDropzone() 
-    {
-        m_currentDisplayRef = null;
+        m_currentDropzone = dropzone.DisplayUI;
     }
     public void OnThoughtDrag(EventInfo info)
     {
-        if (m_currentDisplayRef == null || 
+        if (m_currentDropzone == null || 
             info == null || info.Payload == null) return;
         if (info.Payload.TryGetValue("thought", out object result) &&
             result is DraggableObject drag)
         {
             m_dragging = drag;
             m_dragCancel?.Enable();
-            m_currentDisplayRef?.Show();
+            m_currentDropzone?.Show();
             m_thoughtInventory?.Hide();
         }
     }
     public void OnThoughtDrop() 
     {
+        if(m_currentDropzone != null) 
+        {
+            m_currentDropzone?.Hide();
+            m_thoughtInventory?.Hide();
+        }
         m_dragCancel.Disable();
-        m_currentDisplayRef?.Hide();
-        m_thoughtInventory?.Hide();
         m_dragging = null;
+        m_currentDropzone = null;
     }
     public void OnCacnelDrag()
     {
