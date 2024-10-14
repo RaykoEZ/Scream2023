dbf1f52e745fb1d36ccd5433ef7ba655b827bacf
RaykoEZ <onion55zh@gmail.com>
2024-10-14 05:36:27 +0100

    Editing secret route sequence

---
+++ KeepQuiet/Assets/src/SceneTransition/SecretPrompt.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.InputSystem;
+public class SecretPrompt : MonoBehaviour 
+{
+    [SerializeField] TemporaryInputAction m_secretInput = default;
+    [SerializeField] UnityEvent m_reconnect = default;
+    [SerializeField] UnityEvent m_disconnect = default;
+    public void OnPromptTrigger(InputAction.CallbackContext c) 
+    {
+        // did player press n/y?
+        bool reconnect = c.action.ReadValue<bool>();
+        if (reconnect) 
+        {
+            m_reconnect?.Invoke();
+        }
+        else 
+        {
+            m_disconnect?.Invoke();
+        }
+    }
+    public void ActivatePrompt() 
+    {
+        m_secretInput?.Enable();
+    }
+    void Start()
+    {
+        m_secretInput?.Disable();
+    }
+}
