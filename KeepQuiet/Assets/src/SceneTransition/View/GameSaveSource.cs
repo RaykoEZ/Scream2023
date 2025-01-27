a9ea1c7d23efc3470b86b5873629fe6a50a5ce35
RaykoEZ <onion55zh@gmail.com>
2025-01-27 04:06:58 +0000

    Added save data source interface for future merge with alternate game mode

--- KeepQuiet/Assets/src/SceneTransition/View/GameSaveSource.cs
+++ KeepQuiet/Assets/src/SceneTransition/View/GameSaveSource.cs
@@ -1,18 +1,13 @@
 ﻿using UnityEngine;
 using UnityEngine.Events;
+
 // stores current writable save data, saves game to file/cache
-public class GameSaveSource : MonoBehaviour 
+public class GameSaveSource : BaseSaveSource<SaveData>
 {
     [SerializeField] GameStateFileHandler m_saveState = default;
     [SerializeField] UnityEvent m_onSaveGame = default; 
-    SaveData m_currentGameState;
-    public SaveData Current => m_currentGameState;
-    public void Init(SaveData save)
-    {
-        m_currentGameState = save;
-    }
     // Update game save cache, may save to file if needed
-    public void UpdateSave(bool saveToFile = false)
+    public override void UpdateSave(bool saveToFile = false)
     {
         // Call all save data updates before saving the game
         m_onSaveGame?.Invoke();
---
+++ KeepQuiet/Assets/src/SceneTransition/View/IDataSource.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public interface IDataSource<T> 
+{ 
+    public T Current { get; }
+    public void Init(T save);
+    public void UpdateSave(bool saveToFile = false);
+
+}
+public abstract class BaseSaveSource<T> : MonoBehaviour, IDataSource<T>
+{
+    protected T m_currentGameState;
+    public T Current => m_currentGameState;
+    public virtual void Init(T save) 
+    {
+        m_currentGameState = save;
+    }
+    public abstract void UpdateSave(bool saveToFile = false);
+}
